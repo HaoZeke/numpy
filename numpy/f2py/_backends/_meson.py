16bcd2211fd3d6025902b57ca68664c503e10db0
Andrea <andrea_bianchi@outlook.com>
2023-12-03 14:29:59 +0100

    fixed indentation

--- numpy/f2py/_backends/_meson.py
+++ numpy/f2py/_backends/_meson.py
@@ -77,23 +77,23 @@ class MesonTemplate:
             [
                 f"lib_dir_{i} = declare_dependency(link_args : ['-L{lib_dir}'])"
                 for i, lib_dir in enumerate(self.library_dirs)
-                ]
-            )
+            ]
+        )
 
         self.substitutions["lib_declarations"] = "\n".join(
             [
                 f"{lib} = declare_dependency(link_args : ['-l{lib}'])"
                 for lib in self.libraries
-                ]
-            )
+            ]
+        )
 
         indent = " " * 21
         self.substitutions["lib_list"] = f"\n{indent}".join(
-                [f"{lib}," for lib in self.libraries]
-                )
+            [f"{lib}," for lib in self.libraries]
+        )
         self.substitutions["lib_dir_list"] = f"\n{indent}".join(
-                [f"lib_dir_{i}," for i in range(len(self.library_dirs))]
-                )
+            [f"lib_dir_{i}," for i in range(len(self.library_dirs))]
+        )
 
     def generate_meson_build(self):
         for node in self.pipeline:
